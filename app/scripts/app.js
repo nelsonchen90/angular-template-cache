4ff5a050e0658badf23689f0a852f39a37be0fea
nelsonchen90 <nelsonchen90@gmail.com>
2016-07-13 23:29:45 -0400

    yoeman to init the app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTemplateCacheApp
+ * @description
+ * # angularTemplateCacheApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTemplateCacheApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTemplateCacheApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTemplateCacheApp
+ */
+angular.module('angularTemplateCacheApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
